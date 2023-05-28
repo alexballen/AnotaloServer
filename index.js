044ff215dc474b82e23d16cb3f24c415f969da15
Alex <alexballen53@gmail.com>
2023-05-28 10:31:45 -0500

    Se configura el server y la conexion a la db

---
+++ index.js
@@ -0,0 +1,9 @@
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const port = process.env.PORT || 3001;
+
+conn.sync({ force: false, alter: false }).then(async () => {
+  server.listen(port, () => {
+    console.log(`Server up in port ${port}`);
+  });
+});
