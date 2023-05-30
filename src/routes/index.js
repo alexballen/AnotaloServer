b7c5121b070fb029e863c1fb53ae8d191daa90f7
Alex <alexballen53@gmail.com>
2023-05-29 20:11:24 -0500

    Se crean las rutas a notesR y a userR

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,10 @@
 const Router = require("express");
+const notesR = require("./notesR.js");
+const userR = require("./userR.js");
 
 const router = Router();
+
+router.use("/notes", notesR);
+router.use("/user", userR);
 
 module.exports = router;
