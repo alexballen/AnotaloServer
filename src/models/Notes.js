4e136faf138edc2bdb6596a67aa7341bffb5eca7
Alex <alexballen53@gmail.com>
2023-05-29 20:09:26 -0500

    Se crea la tabla Notes con sus campos

---
+++ src/models/Notes.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("notes", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    importance: {
+      type: DataTypes.ENUM("high", "medium", "low"),
+      allowNull: false,
+    },
+  });
+};
