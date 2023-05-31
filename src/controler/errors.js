7d308099f1892f0992cd41ed347f17ed8ce11431
Alex <alexballen53@gmail.com>
2023-05-30 20:28:28 -0500

    Se crea archivo de constates de errores

---
+++ src/controler/errors.js
@@ -0,0 +1,9 @@
+const correo_ya_existe = "correo_ya_existe";
+const correo_no_existe = "correo_no_existe";
+const ha_ocurrido_un_error = "ha_ocurrido_un_error";
+
+module.exports = {
+  correo_ya_existe,
+  correo_no_existe,
+  ha_ocurrido_un_error,
+};
