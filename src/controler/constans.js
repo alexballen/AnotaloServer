a2a8d1cab0bff8b853ff9f84d093551aee89ea7e
Alex <alexballen53@gmail.com>
2023-05-31 20:33:17 -0500

    Se crean las constantes de los templates y de los errores

---
+++ src/controler/constans.js
@@ -0,0 +1,29 @@
+const errorConstans = {
+  correo_ya_existe: "correo_ya_existe",
+  correo_no_existe: "correo_no_existe",
+  ha_ocurrido_un_error: "ha_ocurrido_un_error",
+};
+
+const templateConstans = {
+  singUp: "sing_up.ejs",
+  singIn: "sing_in.ejs",
+};
+
+module.exports = {
+  errorConstans,
+  templateConstans,
+};
+
+/* {
+  "greeting":"Hola querido usuario",
+  "subject": "Su pago esta pendiente",
+  "email":"arcario55@hotmail.com",
+  "message": "Hola esto es un mesaje de prueba para verificar el funcionamiento de nodemailer, pero pague..."
+} */
+
+/* {
+  "greeting":"Bienvenido",
+  "subject": "Registro exitoso",
+  "email":"arcario55@hotmail.com",
+  "message": "Su registro fue completado satisfactoriamente..."
+} */
