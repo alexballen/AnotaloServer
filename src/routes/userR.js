2a4da8644cf3ec36562157dcbd94192bd3f0218d
Alex <alexballen53@gmail.com>
2023-05-31 20:34:06 -0500

    Se crea ruta post de Email

--- src/routes/userR.js
+++ src/routes/userR.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { getAllUser, postUser } = require("../controler/userC.js");
+const { getAllUser, postUser, postSendMail } = require("../controler/userC.js");
 
 const router = Router();
 
 router.get("/", getAllUser);
+router.post("/email", postSendMail);
 router.post("/", postUser);
 
 module.exports = router;
