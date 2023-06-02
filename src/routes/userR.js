3aade70935f7df1b52c8c62d192f2dcca66370d2
Alex <alexballen53@gmail.com>
2023-06-01 19:56:35 -0500

    Se modifican las rutas y nombres del userR

--- src/routes/userR.js
+++ src/routes/userR.js
@@ -1,10 +1,16 @@
 const { Router } = require("express");
-const { getAllUser, postUser, postSendMail } = require("../controler/userC.js");
+const {
+  getAllUser,
+  postSignUp,
+  postSignIn,
+  postSendMail,
+} = require("../controler/userC.js");
 
 const router = Router();
 
-router.get("/", getAllUser);
+router.get("/user", getAllUser);
+router.post("/signup", postSignUp);
+router.post("/signin", postSignIn);
 router.post("/email", postSendMail);
-router.post("/", postUser);
 
 module.exports = router;
