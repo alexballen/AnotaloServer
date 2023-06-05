ca09a3372dac3694fd1dd703b0e5d8d3a9d9992d
Alex <alexballen53@gmail.com>
2023-06-04 20:50:22 -0500

    Se crean los servicios para la autenticacion con google

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const axios = require("axios");
 const { authConstans } = require("../controler/constans.js");
 
 const emailExists = async (UserDb, email) => {
@@ -117,7 +118,79 @@ const SignIn = async (UserDb, email, password, res) => {
   }
 };
 
+const signInGoogle = async () => {
+  try {
+    const CLIENT_ID =
+      "851275819483-94ma132th8ejk8fdstkhtur9e0ap3stg.apps.googleusercontent.com";
+    const CLIENT_SECRET = "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";
+    const REDIRECT_URI = "http://localhost:3001/auth/google/callback";
+
+    const authorizationUrl = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=profile email`;
+
+    return authorizationUrl;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const googleAuthorizationCode = (req) => {
+  try {
+    const { code } = req.query;
+    return code || null;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAccessToken = async (code) => {
+  try {
+    const CLIENT_ID =
+      "851275819483-94ma132th8ejk8fdstkhtur9e0ap3stg.apps.googleusercontent.com";
+    const CLIENT_SECRET = "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";
+    const REDIRECT_URI = "http://localhost:3001/auth/google/callback";
+    const tokenResponse = await axios.post(
+      "https://oauth2.googleapis.com/token",
+      {
+        code,
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        redirect_uri: REDIRECT_URI,
+        grant_type: "authorization_code",
+      }
+    );
+
+    return tokenResponse.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const otrocodigo = async () => {
+  const accessToken = tokenResponse.data.access_token;
+  console.log(accessToken);
+
+  const userInfoResponse = await axios.get(
+    "https://www.googleapis.com/oauth2/v2/userinfo",
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+
+  console.log(userInfoResponse);
+
+  // Aquí puedes utilizar los datos de usuario en userInfoResponse.data
+  const { id, email, name, picture } = userInfoResponse.data;
+  // Realiza acciones adicionales con los datos del usuario, como crear una sesión, guardar en la base de datos, etc.
+  // Redirige al usuario a la página de inicio o a donde desees
+  res.redirect("/dashboard");
+};
+
 module.exports = {
   SignUp,
   SignIn,
+  signInGoogle,
+  googleAuthorizationCode,
+  getAccessToken,
 };
