2c072e2c4b6c1172802460d6cbc0fc8baab0fd6a
Alex <alexballen53@gmail.com>
2023-06-04 20:48:17 -0500

    Se crean unas rutas para la autenticacion con google

--- src/routes/userR.js
+++ src/routes/userR.js
@@ -4,11 +4,15 @@ const {
   postSignUp,
   postSignIn,
   postSendMail,
+  getSingInGoogle,
+  getCodeAuthGoogle,
 } = require("../controler/userC.js");
 
 const router = Router();
 
 router.get("/user", getAllUser);
+router.get("/auth/google/callback", getCodeAuthGoogle);
+router.get("/auth/google", getSingInGoogle);
 router.post("/signup", postSignUp);
 router.post("/signin", postSignIn);
 router.post("/email", postSendMail);
