b5501492f52f5aa320467d331cc4f2c2c0ca658c
Alex <alexballen53@gmail.com>
2023-06-05 20:32:11 -0500

    Se añade ruta de errores

--- src/routes/userR.js
+++ src/routes/userR.js
@@ -6,13 +6,15 @@ const {
   postSendMail,
   getSingInGoogle,
   getCodeAuthGoogle,
+  getErrorAuthGoogle,
 } = require("../controler/userC.js");
 
 const router = Router();
 
 router.get("/user", getAllUser);
-router.get("/auth/google/callback", getCodeAuthGoogle);
 router.get("/auth/google", getSingInGoogle);
+router.get("/auth/google/callback", getCodeAuthGoogle);
+router.get("/auth/error/v2", getErrorAuthGoogle);
 router.post("/signup", postSignUp);
 router.post("/signin", postSignIn);
 router.post("/email", postSendMail);
