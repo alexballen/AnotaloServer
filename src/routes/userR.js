681493efe5a0f897220afd273438f87ca890d4f2
Alex <alexballen53@gmail.com>
2023-08-03 11:16:27 -0500

    Se crean las rutas resetPassword y passwordResetConfirmation para la restauracion de contraseñas

--- src/routes/userR.js
+++ src/routes/userR.js
@@ -6,6 +6,8 @@ const {
   getSingInGoogle,
   getAccessTokenGoogle,
   verificationToken,
+  resetPassword,
+  passwordResetConfirmation,
   tokenConfirmation,
   deleteUser,
 } = require("../controler/userC.js");
@@ -18,6 +20,8 @@ router.post("/signin", getAccessTokenGoogle);
 router.post("/signup", postSignUp);
 router.post("/email", postSendMail);
 router.post("/verification", verificationToken);
+router.post("/resetpassword", resetPassword);
+router.post("/passwordresetconfirmation", passwordResetConfirmation);
 router.patch("/accountconfirmation", tokenConfirmation);
 router.delete("/delete/:userId", deleteUser);
 
